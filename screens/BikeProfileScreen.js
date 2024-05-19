db0e03e75cfd789dbffc30aa3cfd98065a315d3d
muwongelawrence-tech <muwongelawrence44@gmail.com>
2024-05-19 17:37:56 +0300

    ui clean up phase 1

--- screens/BikeProfileScreen.js
+++ screens/BikeProfileScreen.js
@@ -5,88 +5,88 @@ import { ArrowUpOnSquareStackIcon, ChevronLeftIcon, PencilIcon } from 'react-nat
 
 
 
-const BikeProfileScreen = ({navigation}) => {
+const BikeProfileScreen = ({ navigation }) => {
   return (
-  <SafeAreaView className='bg-white h-full'>
-    <View className=''>
-    <View className='flex-row items-center relative mt-6 justify-center mb-3'>
-    <Pressable className='absolute left-4'
-    
-     onPress={navigation.getBacK()}>
-    <ChevronLeftIcon size={30} color='#242424'/>
-    </Pressable>
-    <Text className='text-[17px] font-[600] text-[#242424]'>
-    Notifications
-    </Text>
-    </View>
-    <View className="border-[#E0E0E0] border-[0.5px] mt-2"></View>
-    <View className='items-center'>
-   
-     <View className='w-96   bg-[#FAFAFA] rounded-[12px] mt-5'>
-      <Pressable
-      onPress={()=>navigation.navigate('BikeManufactureDetailsScreen')}
-      
-       className='m-5 flex-row justify-between items-center'>
-     <View>
-     <Text className='text-[17px] font-[400]  text-[#242424]'>
-     Bike manufacturer
-      </Text>
-      <Text className='text-[#616161] text-[13px] font-[400]'>
-      Value
-      </Text>
-     </View>
-     <Pressable>
-     <PencilIcon  size={26} color='#808080'/>
+    <SafeAreaView className='bg-white h-full'>
+      <View className=''>
+        <View className='flex-row items-center relative mt-6 justify-center mb-3'>
+          <Pressable className='absolute left-4'
 
-       
-     </Pressable>
-     
-      
-     </Pressable>
-     
+            onPress={navigation.goBack()}>
+            <ChevronLeftIcon size={30} color='#242424' />
+          </Pressable>
+          <Text className='text-[17px] font-[600] text-[#242424]'>
+            Notifications
+          </Text>
+        </View>
+        <View className="border-[#E0E0E0] border-[0.5px] mt-2"></View>
+        <View className='items-center'>
 
-     <View className='m-5 flex-row justify-between items-center'>
-     <View>
-     <Text className='text-[17px] font-[400]  text-[#242424]'>
-     Battery capacity
-      </Text>
-      <Text className='text-[#616161] text-[13px] font-[400]'>
-      Value
-      </Text>
-     </View>
-     <Pressable>
-     <PencilIcon  size={26} color='#808080'/>
+          <View className='w-96   bg-[#FAFAFA] rounded-[12px] mt-5'>
+            <Pressable
+              onPress={() => navigation.navigate('BikeManufactureDetailsScreen')}
 
-       
-     </Pressable>
-     
-      
-     </View>
-     
-     <View className='m-5 flex-row justify-between items-center'>
-     <View>
-     <Text className='text-[17px] font-[400]  text-[#242424]'>
-     Number plate
-      </Text>
-      <Text className='text-[#616161] text-[13px] font-[400]'>
-      Value
-      </Text>
-     </View>
-     <Pressable>
-     <PencilIcon  size={26} color='#808080'/>
-
-       
-     </Pressable>
-     
-      
-     </View>
-     
+              className='m-5 flex-row justify-between items-center'>
+              <View>
+                <Text className='text-[17px] font-[400]  text-[#242424]'>
+                  Bike manufacturer
+                </Text>
+                <Text className='text-[#616161] text-[13px] font-[400]'>
+                  Value
+                </Text>
+              </View>
+              <Pressable>
+                <PencilIcon size={26} color='#808080' />
 
 
-    </View>
-   </View>
-    </View>
-   </SafeAreaView>
+              </Pressable>
+
+
+            </Pressable>
+
+
+            <View className='m-5 flex-row justify-between items-center'>
+              <View>
+                <Text className='text-[17px] font-[400]  text-[#242424]'>
+                  Battery capacity
+                </Text>
+                <Text className='text-[#616161] text-[13px] font-[400]'>
+                  Value
+                </Text>
+              </View>
+              <Pressable>
+                <PencilIcon size={26} color='#808080' />
+
+
+              </Pressable>
+
+
+            </View>
+
+            <View className='m-5 flex-row justify-between items-center'>
+              <View>
+                <Text className='text-[17px] font-[400]  text-[#242424]'>
+                  Number plate
+                </Text>
+                <Text className='text-[#616161] text-[13px] font-[400]'>
+                  Value
+                </Text>
+              </View>
+              <Pressable>
+                <PencilIcon size={26} color='#808080' />
+
+
+              </Pressable>
+
+
+            </View>
+
+
+
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
--- screens/ProfileUser.js
+++ screens/ProfileUser.js
@@ -1,70 +1,68 @@
 import { View, Text, SafeAreaView, Pressable } from 'react-native'
 import React, { useState } from 'react'
-import { ArrowDownLeftIcon, ChevronRightIcon, UserCircleIcon } from 'react-native-heroicons/solid'
+import { ChevronRightIcon, UserCircleIcon } from 'react-native-heroicons/solid'
 import { ArrowUpOnSquareStackIcon, ChevronLeftIcon } from 'react-native-heroicons/outline'
 import DeletAccountModel from '../components/DeletAccountModel'
-import { Tuple } from '@reduxjs/toolkit'
 
-const ProfileUser = ({navigation}) => {
-const [visible, setVisible] = useState(false)
-const onCloseModle =()=>{
-  console.log('onCloseModle')
-  setVisible(!visible)
+const ProfileUser = ({ navigation }) => {
 
-}
- 
+  const [visible, setVisible] = useState(false);
+
+  const toggleModalVisibility = () => {
+    setVisible(!visible);
+  }
+
   return (
     <SafeAreaView className='bg-white h-full'>
-    <View className=''>
-    <View className='flex-row items-center relative mt-6 justify-center mb-3'>
-    <Pressable className='absolute left-4'>
-    <ChevronLeftIcon size={30} color='#242424'/>
-    </Pressable>
-    <Text className='text-[17px] font-[600] text-[#242424]'>
-      Account info
-    </Text>
-    </View>
-    <View className="border-[#E0E0E0] border-[0.5px] mt-2"></View>
-    <View className='m-5'>
-    <UserCircleIcon size={55}color='#2C7721' />
-     <View className='w-96  h-52 bg-[#FAFAFA] rounded-[12px] mt-5'>
-      <View className='m-5'>
-     <Text className='text-[17px] font-[400]  text-[#242424]'>
-       Phone umber
-      </Text>
-     <Text className='text-[#616161]'>
-       UserNumber
-       </Text>
-      <View className='mb-4 mt-6'>
+      <View className=''>
+        <View className='flex-row items-center relative mt-6 justify-center mb-3'>
+          <Pressable className='absolute left-4'>
+            <ChevronLeftIcon size={30} color='#242424' />
+          </Pressable>
+          <Text className='text-[17px] font-[600] text-[#242424]'>
+            Account info
+          </Text>
+        </View>
+        <View className="border-[#E0E0E0] border-[0.5px] mt-2"></View>
+        <View className='m-5'>
+          <UserCircleIcon size={55} color='#2C7721' />
+          <View className='w-96  h-52 bg-[#FAFAFA] rounded-[12px] mt-5'>
+            <View className='m-5'>
+              <Text className='text-[17px] font-[400]  text-[#242424]'>
+                Phone umber
+              </Text>
+              <Text className='text-[#616161]'>
+                UserNumber
+              </Text>
+              <View className='mb-4 mt-6'>
 
-       <Pressable  onPress={()=> navigation.navigate('BikeProfileScreen')}
-         className='flex-row justify-between'>
-       <View className='flex-row space-x-3'>
-       <ArrowUpOnSquareStackIcon size={30} color='#808080'/ >
-       <Text className='text-[17px] font-[400] text-[#242424]'> Bike   profile </Text>
-       </View>
-       <ChevronRightIcon size={30} color='#808080'/>
-           
-       </Pressable>
-       </View>
+                <Pressable onPress={() => navigation.navigate('BikeProfileScreen')}
+                  className='flex-row justify-between'>
+                  <View className='flex-row space-x-3'>
+                    <ArrowUpOnSquareStackIcon size={30} color='#808080' />
+                    <Text className='text-[17px] font-[400] text-[#242424]'> Bike profile </Text>
+                  </View>
+                  <ChevronRightIcon size={30} color='#808080' />
 
-       <View className='items-center'>
-       <Pressable onPress={onCloseModle} >
-            <Text className='text-[#C50F1F]  text-[17px] font-[400]'>
-        Delete account
-     </Text>
-      </Pressable>
-     </View>
-     </View>
+                </Pressable>
+              </View>
 
+              <View className='items-center bg-red-800'>
+                <Pressable onPress={toggleModalVisibility} >
+                  <Text className='text-[#C50F1F]  text-[17px] font-[400]'>
+                    Delete account
+                  </Text>
+                </Pressable>
+              </View>
+            </View>
 
-    </View>
-   </View>
-    </View>
+          </View>
+        </View>
+      </View>
 
-    <DeletAccountModel  visible={visible}  onCloseModle={onCloseModle}/>
-   </SafeAreaView>
-   
+      <DeletAccountModel visible={visible} toggleModalVisibility={toggleModalVisibility} />
+    </SafeAreaView>
+
   )
 }
 
