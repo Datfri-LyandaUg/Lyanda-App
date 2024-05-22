6a6b60292520c67f84c8bad223ab40296794847a
muwongelawrence-tech <muwongelawrence44@gmail.com>
2024-05-22 15:37:41 +0300

    configuring navigation of the current screens

--- components/CustomSettingButton.js
+++ components/CustomSettingButton.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 
-const CustomSettingButton = ({ HandleOnClick, icon1, icon2, title }) => {
+const CustomSettingButton = ({ onPress, icon1, icon2, title }) => {
   return (
-   
-      <View className='flex-row  items-center justify-between mx-4 space-y-6 '>
-        <View className='flex-row space-x-4 items-center'>
-          {icon1}
-          <Text className='text-[#242424] font-[400]  text-[17px]'>
-            {title}
-          </Text>
-        </View>
-        {icon2}
+
+    <Pressable onPress={onPress} className='flex-row  items-center justify-between mx-4 space-y-6 '>
+      <View className='flex-row space-x-4 items-center'>
+        {icon1}
+        <Text className='text-[#242424] font-[400]  text-[17px]'>
+          {title}
+        </Text>
       </View>
+      {icon2}
+    </Pressable>
 
   )
 }
