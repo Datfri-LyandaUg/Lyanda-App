d956e4edbd4dd67ac0147b8117b3b40cb74904f2
muwongelawrence-tech <muwongelawrence44@gmail.com>
2024-05-27 15:31:35 +0300

    cleaning the faqscreen

--- screens/FaqScreen.js
+++ screens/FaqScreen.js
@@ -21,19 +21,16 @@ const FaqScreen = ({ navigation }) => {
 
       <View className='items-center px-2 mt-4'>
 
-        <View className='w-full bg-[#FAFAFA]  rounded-[12px] space-y-4 py-7'>
+        <View className='w-full bg-[#FAFAFA] rounded-[12px] pb-5'>
 
-          <View className='mx-5 space-y-5s'>
+          <View className='mx-5 space-y-3'>
             <Text className='text-[17px] font-[600]   mt-7 text-[#242424]'>What is the purpose of this app?</Text>
             <Text className='text-[13px] font-[400]  text-[#242424]'>
               This battery ordering app is designed to provide a convenient and efficient way for users to order batteries for their bikes faster and easily.
             </Text>
-
           </View>
 
-          <View
-
-            className='mx-5  space-y-3'>
+          <View className='mx-5  space-y-3'>
 
             <Text className='text-[17px] font-[600]   mt-7 text-[#242424]'>How does the app work?</Text>
             <Text className='text-[13px] font-[400]  text-[#242424]'>
@@ -41,6 +38,7 @@ const FaqScreen = ({ navigation }) => {
             </Text>
 
           </View>
+
           <View className='mx-5  space-y-3'>
             <Text className='text-[17px] font-[600]   mt-7 text-[#242424]'>Are there any discounts or loyalty programs
               available for frequent users?</Text>
@@ -49,23 +47,19 @@ const FaqScreen = ({ navigation }) => {
             </Text>
 
           </View>
-          <View
 
-            className='mx-5  space-y-5'>
+          <View className='mx-5 space-y-3'>
 
-            <Text className='text-[17px] font-[600]   mt-7 text-[#242424]'>Is it safe to provide my payment information
+            <Text className='text-[17px] font-[600] mt-7 text-[#242424]'>Is it safe to provide my payment information
               on this app?</Text>
             <Text className='text-[13px] font-[400]  text-[#242424]'>
               Yes, we take the security of your payment information seriously. Our app uses encryption technology to protect your data, and we do not store sensitive payment details on our servers.</Text>
           </View>
 
-          <View
+          <View className='mx-5 space-y-3'>
 
-            className='mx-5  space-y-5'>
-
-            <Text className='text-[17px] font-[600]   mt-7 text-[#242424]'>Is this app available on both Android and iOS
+            <Text className='text-[17px] font-[600] mt-7 text-[#242424]'>Is this app available on both Android and iOS
               devices?</Text>
-
             <View>
               <Text className='text-[13px] font-[400]  text-[#242424]'>
                 Yes, our app is available for download on both Android and iOS devices.
