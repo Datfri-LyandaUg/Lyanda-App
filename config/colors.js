c02c5083badff1f035cc0ec18cdde085883cfadc
muwongelawrence-tech <muwongelawrence44@gmail.com>
2024-06-04 08:45:33 +0300

    configuring the colors theme

---
+++ config/colors.js
@@ -0,0 +1,19 @@
+export default {
+    light: {
+        primaryButton: '',
+        secondaryButon: '',
+        background: '',
+        container: "",
+        text: '',
+        icon: '',
+        
+    },
+    dark: {
+        primaryButton: '',
+        secondaryButon: '',
+        container: "",
+        background: "",
+        text: '', 
+        icon: '', 
+    },
+};
