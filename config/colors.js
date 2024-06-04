1cc9d3b1ab9ba188478782afd8d3a3e4034d5541
muwongelawrence-tech <muwongelawrence44@gmail.com>
2024-06-04 18:00:15 +0300

    configuring  theme color variables

--- config/colors.js
+++ config/colors.js
@@ -1,19 +1,22 @@
-export default {
+const colors = {
     light: {
-        primaryButton: '',
-        secondaryButon: '',
-        background: '',
-        container: "",
-        text: '',
-        icon: '',
-        
+        primaryButtonText: '#FFFFFF',
+        secondaryButonText: '#2C7721',
+        background: '#FFFFFF',
+        container: "#FAFAFA",
+        headerText: '#242424',
+        text: '#616161',
+        icon: '#808080',
     },
     dark: {
-        primaryButton: '',
-        secondaryButon: '',
-        container: "",
-        background: "",
-        text: '', 
-        icon: '', 
+        primaryButtonText: '#000000',
+        secondaryButonText: '#319726',
+        container: "#424242",
+        background: "#1F1F1F",
+        headerText: '#FFFFFF',
+        text: '#D6D6D6',
+        icon: '#ADADAD',
     },
 };
+
+export default colors;
